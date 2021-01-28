3bb308ec5c6a500a5d5b55a03ad3d3095d0be11b
Tim Scott <0timothy.scott0@gmail.com>
2021-01-27 20:57:01 -0500

    add intolerance list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,49 @@
 $(function () {
 	$('.chips').chips();
+	$('select').formSelect();
 
+	// prevent form submission when adding ingredient chip
 	$('form .chips input').keydown(function (e) {
 		if (e.keyCode == 13) {
 			e.preventDefault();
 			return false;
 		}
 	});
+
+	let intolerances = [
+		"Dairy",
+		"Egg",
+		"Gluten",
+		"Grain",
+		"Peanut",
+		"Seafood",
+		"Sesame",
+		"Shellfish",
+		"Soy",
+		"Sulfite",
+		"Tree Nut",
+		"Wheat"
+	];
+
+	let intoleranceArea = $(".intoleranceArea");
+
+	// populate intolerance list 
+	intolerances.forEach(allergy => {
+		let inputID = "I" + allergy;
+		intoleranceArea
+			.append($("<p>")
+				.addClass("col s12 m6 xl4")
+				.append($("<label>")
+					.attr("for", inputID)
+					.append($("<input>")
+						.attr("id", inputID)
+						.attr("type", "checkbox")
+					)
+					.append($("<span>")
+						.text(allergy)
+					)
+				)
+			);
+	});
+
 });
