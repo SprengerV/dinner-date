48c639249691a62c20ef70dc258828b332a80351
Hanna Munoz <hannamunoz@gmail.com>
2021-01-27 20:14:04 -0500

    'first commit'

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+let urlQuery = "https://api.spoonacular.com/recipes/complexSearch"
+const apiKey = '38f510f9fde94594b09c5518393c289c'
+
+// variables to store list of ingredients 
+let ingredients = document.getElementById('');
+// variable to store diet type (vegetarian, ect.)
+let dietType = document.getElementById('');
+// variable for intolerances (gluten, ect.) this will be a drop-down or check box
+let intolerances = document.getElementById('');
+// variable for search button
+let searchBtn = document.getElementById('');
+// variable for materialize chips functionality
+var chip = {
+    tag: 'chip content',
+    image: '', //optional
+  };
+
+// materialize chips functionality
+$('.chips').chips();
+$('.chips-initial').chips({
+  data: [{
+    tag: 'Apple',
+  }, {
+    tag: 'Microsoft',
+  }, {
+    tag: 'Google',
+  }],
+});
+$('.chips-placeholder').chips({
+  placeholder: 'Enter a tag',
+  secondaryPlaceholder: '+Tag',
+});
+$('.chips-autocomplete').chips({
+  autocompleteOptions: {
+    data: {
+      'Apple': null,
+      'Microsoft': null,
+      'Google': null
+    },
+    limit: Infinity,
+    minLength: 1
+  }
+});
+
+// need an onclick event for search button
+$(searchBtn).on('click',);
+
+// need a function to string text from ingredient input to add to api request
+
+// need a function to autocomplete ingredient items
+
+
+// need a function to gather ingredients for API request
+function searchByIngredient() {
+    let queryURL = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='+ingredients;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+// need a function to gather diet type
+function searchByDiet() {
+    let queryURL = 'https://api.spoonacular.com/recipes/complexSearch?diet='+dietType;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+// need a function to gather intolerances
+
