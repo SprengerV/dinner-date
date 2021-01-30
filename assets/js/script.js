99ad1baf2dbe17cc14ae2df55aa079136b0ebe21
Tim Scott <0timothy.scott0@gmail.com>
2021-01-30 10:52:58 -0500

    food selection form submission & data retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,38 @@
 $(function () {
-	$('.chips').chips();
-	$('select').formSelect();
 
-	// prevent form submission when adding ingredient chip
-	$('form .chips input').keydown(function (e) {
-		if (e.keyCode == 13) {
-			e.preventDefault();
-			return false;
-		}
-	});
+	// meal search submission
+	$(".mealSearchForm").on("submit", function (e) {
+		e.preventDefault()
 
-	let intolerances = [
-		"Dairy",
-		"Egg",
-		"Gluten",
-		"Grain",
-		"Peanut",
-		"Seafood",
-		"Sesame",
-		"Shellfish",
-		"Soy",
-		"Sulfite",
-		"Tree Nut",
-		"Wheat"
-	];
+		// prepare data object
+		let dataObj = {
+			ingredients: [],
+			intolerances: []
+		};
 
-	let intoleranceArea = $(".intoleranceArea");
+		// form jquery object
+		let form = $(this);
+		let formData = form.serializeArray();
 
-	// populate intolerance list 
-	intolerances.forEach(allergy => {
-		let inputID = "I" + allergy;
-		intoleranceArea
-			.append($("<p>")
-				.addClass("col s12 m6 xl4")
-				.append($("<label>")
-					.attr("for", inputID)
-					.append($("<input>")
-						.attr("id", inputID)
-						.attr("type", "checkbox")
-					)
-					.append($("<span>")
-						.text(allergy)
-					)
-				)
-			);
+		// access chips data
+		let chipElement = form.children(".chips").eq(0)
+		let chipInstance = M.Chips.getInstance(chipElement);
+
+		// add chip data to dataObj
+		chipInstance.chipsData.forEach((chip) => {
+			dataObj.ingredients.push(chip.tag.toLowerCase());
+		});
+
+		// add intolerance data to dataObj
+		formData.forEach(value => {
+			// check value is and intolerance
+			if (value.name[0] === "I") {
+				dataObj.intolerances.push(value.name.slice(1).toLowerCase());
+			}
+		});
+
+		// all form data is in dataObj now
+		console.log(dataObj);
 	});
 
 });
