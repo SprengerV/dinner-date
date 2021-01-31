37692cf7b3101336060f67d843ee282a6c2628c6
SprengerV <b.francis@sprengerv.com>
2021-01-31 13:38:14 -0500

    creation of genres URL

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -2,12 +2,28 @@ movAPIkey = 'f66fd70d918aed123c6a3b422a1934d8';
 v4AuthToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjZmZDcwZDkxOGFlZDEyM2M2YTNiNDIyYTE5MzRkOCIsInN1YiI6IjYwMTA5OGJjZWE4NGM3MDAzYmI5ZDg2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HBK_9SRA3SUJcAuFJLvJ24OnQ1rCmSASqtniQCO3ayE';
 let genre = 'comedy';
 const searchBy = '&query='+ genre;
-const genresURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' + movAPIkey + '&page=2'
-let genreID = '10751'
-const discover = 'https://api.themoviedb.org/3/discover/movie?api_key=' + movAPIkey + ''
+const genresURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' + movAPIkey + '&page=1';
+let genreID = '10751';
+
+
+$('#submit').on('click',() => {
+    var genres = $('.genre:checked').map(function(){
+        return this.val();
+    }).join('|');
+    const discover = 'https://api.themoviedb.org/3/discover/movie?api_key=' + movAPIkey + '&with_genres=' + genres + '&include_adult=false&with_original_language=en';
+    console.log(discover);
+
+});
+
+var genres = $('.genre:checked').map(function(){
+    return this.val();
+}).join('|');
+const discover = 'https://api.themoviedb.org/3/discover/movie?api_key=' + movAPIkey + '&with_genres=' + genres + '&include_adult=false&with_original_language=en';
+console.log(discover);
 
 $(window).on('load', function(){
-    $.get(genresURL).then(function(response){
+    $.get(discover).then(function(response){
         console.log(response);
     });
+
 });
