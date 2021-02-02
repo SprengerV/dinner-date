d79565f93d8cff1b2acc06b96af13508a554cc84
SprengerV <b.francis@sprengerv.com>
2021-02-01 21:07:48 -0500

    movie data push to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,15 +64,21 @@ $(function () {
 		const discover = 'https://api.themoviedb.org/3/discover/movie?api_key=f66fd70d918aed123c6a3b422a1934d8&include_adult=false&with_original_language=en' + genres;
 		$.get(discover).then(function(response){
 			console.log(response);
+			// pick a random movie from the response list
+			const rand = Math.floor(Math.random()*response.results.length);
+			const pick = response.results[rand];
+			// set up displayMovie()
+			let movie = {
+				posterSrc: "",
+				name: "",
+				summary: ""
+			};
+			movie.posterSrc = 'https://image.tmdb.org/t/p/w500' + pick.poster_path;
+			movie.name = pick.title;
+			movie.summary = pick.overview;
+			// display movie
+			displayMovie(movie);
 		});
-		// set up displayMovie()
-		let movie = {
-			posterSrc: "",
-			name: "",
-			summary: ""
-		};
-		// display movie
-		// displayMovie(movie);
 	});
 
 	function displayMovie(movieObj) {
